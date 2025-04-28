0a92952b398f0afbd5e8884a33a503409efee7d9
rsalamow <rsalamow@gmail.com>
2025-04-29 01:07:10 +0300

    added ParseMortgageRates method that is usable for blue prism invoke code stage

--- EquitabileBankDemo/Program.cs
+++ EquitabileBankDemo/Program.cs
@@ -4,6 +4,8 @@ using HtmlAgilityPack;
 
 Console.WriteLine("Hello, World!");
 var url = "https://www.equitablebank.ca/mortgage-rates";
+DataTable rates;
+ParseMortgageRates(url, out rates);
 
 using (HttpClient client = new HttpClient())
 {
@@ -50,5 +52,44 @@ using (HttpClient client = new HttpClient())
             Console.Write($"{row[col]}\t");
         }
         Console.WriteLine();
+    }
+}
+
+static void ParseMortgageRates(string url, out DataTable dataTable)
+{
+    dataTable = new DataTable();
+
+    using (HttpClient client = new HttpClient())
+    {
+        var html = client.GetStringAsync(url).GetAwaiter().GetResult();
+
+        HtmlDocument document = new HtmlDocument();
+        document.LoadHtml(html);
+
+        var tableNode = document.DocumentNode.SelectSingleNode("//table[contains(@class, 'eq-table') and contains(@class, 'k-table')]");
+
+        if (tableNode == null)
+            throw new Exception("Table not found.");
+
+        var headerNodes = tableNode.SelectNodes(".//tr[1]/th");
+        foreach (var headerNode in headerNodes)
+        {
+            dataTable.Columns.Add(headerNode.InnerText.Trim());
+        }
+
+        var rowNodes = tableNode.SelectNodes(".//tr[position() > 1]");
+
+        foreach (var rowNode in rowNodes)
+        {
+            DataRow dataRow = dataTable.NewRow();
+            var cellNodes = rowNode.SelectNodes("./th|./td");
+
+            for (int i = 0; i < cellNodes.Count && i < dataTable.Columns.Count; i++)
+            {
+                dataRow[i] = cellNodes[i].InnerText.Trim();
+            }
+
+            dataTable.Rows.Add(dataRow);
+        }
     }
 }
