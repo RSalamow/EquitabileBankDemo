b591c052b8231d7d2e3682f3cb094228516dcb5c
rsalamow <rsalamow@gmail.com>
2025-05-16 11:35:22 +0300

    created a new class to get the rates

---
+++ EquitabileBankDemo/Helpers/RatesScraper.cs
@@ -0,0 +1,119 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquitabileBankDemo.Helpers
+{
+    public class RatesScraper
+    {
+        public string Url { get; set; }
+        private string _HtmlContent;
+
+        public RatesScraper(string url)
+        {
+            Url = url;
+        }
+
+        public async Task<string> GetHtmlContentAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                _HtmlContent = await client.GetStringAsync(Url);
+            }
+            return _HtmlContent;
+        }
+
+        public DataTable ParseMortgageRates(string html)
+        {
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(html);
+            var tableNode = document.DocumentNode.SelectSingleNode("//table[contains(@class, 'eq-table') and contains(@class, 'k-table')]");
+            if (tableNode == null)
+            {
+                Console.WriteLine("Table not found.");
+                return null;
+            }
+            DataTable dataTable = new DataTable();
+            var headerNodes = tableNode.SelectNodes(".//tr[1]/th");
+            foreach (var headerNode in headerNodes)
+            {
+                dataTable.Columns.Add(headerNode.InnerText.Trim());
+            }
+            var rowNodes = tableNode.SelectNodes(".//tr[position() > 1]");
+            foreach (var rowNode in rowNodes)
+            {
+                DataRow dataRow = dataTable.NewRow();
+                var cellNodes = rowNode.SelectNodes("./th|./td");
+                for (int i = 0; i < cellNodes.Count && i < dataTable.Columns.Count; i++)
+                {
+                    dataRow[i] = cellNodes[i].InnerText.Trim();
+                }
+                dataTable.Rows.Add(dataRow);
+            }
+            return dataTable;
+        }
+
+        public DataTable GetMortgageFlexRates(string html, RateType rateType)
+        {
+            DataTable ratesTable = new DataTable();
+            var selectedRate = "";
+            switch (rateType)
+            {
+                case RateType.Flex:
+                    selectedRate = "Reverse Mortgage Flex Rate";
+                    break;
+                case RateType.FlexPLUS:
+                    selectedRate = "Reverse Mortgage Flex PLUS Rate";
+                    break;
+                case RateType.FlexLite:
+                    selectedRate = "Reverse Mortgage Flex Lite Rate";
+                    break;
+            }
+
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(html);
+
+            var tableNodes = document.DocumentNode.SelectNodes("//table[contains(@class, 'eq-table rates-table k-table')]");
+            var tableNode = tableNodes 
+                ?.FirstOrDefault(node => node.InnerText.Contains(selectedRate, StringComparison.OrdinalIgnoreCase));
+
+            // Rates table
+            if (tableNode == null)
+                throw new Exception("Rates table not found.");
+
+            var headerNodes = tableNode.SelectNodes(".//tr[1]/th");
+            foreach (var headerNode in headerNodes)
+            {
+                ratesTable.Columns.Add(headerNode.InnerText.Trim());
+            }
+
+            var rowNodes = tableNode.SelectNodes(".//tr[position() > 1]");
+
+            foreach (var rowNode in rowNodes)
+            {
+                DataRow dataRow = ratesTable.NewRow();
+                var cellNodes = rowNode.SelectNodes("./th|./td");
+
+                for (int i = 0; i < cellNodes.Count && i < ratesTable.Columns.Count; i++)
+                {
+                    dataRow[i] = cellNodes[i].InnerText.Trim().Replace('\u00A0', ' ').Trim();
+                }
+
+                ratesTable.Rows.Add(dataRow);
+            }
+            return ratesTable;
+        }
+
+    }
+
+    public enum RateType
+    {
+        Flex,
+        FlexPLUS,
+        FlexLite
+    }
+}
--- EquitabileBankDemo/Program.cs
+++ EquitabileBankDemo/Program.cs
@@ -1,11 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Data;
+using EquitabileBankDemo.Helpers;
 using HtmlAgilityPack;
 
 Console.WriteLine("Hello, World!");
 var url = "https://www.equitablebank.ca/mortgage-rates";
+
+var ratesScraper = new RatesScraper(url);
+var htmlContent = await ratesScraper.GetHtmlContentAsync();
+var ratesTable = ratesScraper.ParseMortgageRates(htmlContent);
+var flexRatesTable = ratesScraper.GetMortgageFlexRates(htmlContent, RateType.Flex);
+var flexPlusRatesTable = ratesScraper.GetMortgageFlexRates(htmlContent, RateType.FlexPLUS);
+var flexLiteRatesTable = ratesScraper.GetMortgageFlexRates(htmlContent, RateType.FlexLite);
+
+
 DataTable rates;
 ParseMortgageRates(url, out rates);
+
+
+
 
 using (HttpClient client = new HttpClient())
 {
@@ -59,6 +72,7 @@ static void ParseMortgageRates(string url, out DataTable dataTable)
 {
     dataTable = new DataTable();
 
+
     using (HttpClient client = new HttpClient())
     {
         var html = client.GetStringAsync(url).GetAwaiter().GetResult();
@@ -67,9 +81,18 @@ static void ParseMortgageRates(string url, out DataTable dataTable)
         document.LoadHtml(html);
 
         var tableNode = document.DocumentNode.SelectSingleNode("//table[contains(@class, 'eq-table') and contains(@class, 'k-table')]");
+        var tableNodes = document.DocumentNode.SelectNodes("//table[contains(@class, 'eq-table rates-table k-table')]");
 
+        var tableNodeFlex = tableNodes
+            ?.FirstOrDefault(node => node.InnerText.Contains("Reverse Mortgage Flex Rate", StringComparison.OrdinalIgnoreCase));
+        var tableNodeFlexPLUS = tableNodes
+            ?.FirstOrDefault(node => node.InnerText.Contains("Reverse Mortgage Flex PLUS Rate", StringComparison.OrdinalIgnoreCase));
+        var tableNodeFlexLite = tableNodes
+            ?.FirstOrDefault(node => node.InnerText.Contains("Reverse Mortgage Flex Lite Rate", StringComparison.OrdinalIgnoreCase));
+        
+        // Rates table
         if (tableNode == null)
-            throw new Exception("Table not found.");
+            throw new Exception("Rates table not found.");
 
         var headerNodes = tableNode.SelectNodes(".//tr[1]/th");
         foreach (var headerNode in headerNodes)
@@ -86,10 +109,63 @@ static void ParseMortgageRates(string url, out DataTable dataTable)
 
             for (int i = 0; i < cellNodes.Count && i < dataTable.Columns.Count; i++)
             {
-                dataRow[i] = cellNodes[i].InnerText.Trim();
+                dataRow[i] = cellNodes[i].InnerText.Trim().Replace('\u00A0', ' ').Trim();
             }
 
             dataTable.Rows.Add(dataRow);
         }
+
+        // Rates table Flex
+        if (tableNode == null)
+            throw new Exception("Rates Flex table not found.");
+
+        headerNodes = tableNode.SelectNodes(".//tr[1]/th");
+        foreach (var headerNode in headerNodes)
+        {
+            dataTable.Columns.Add(headerNode.InnerText.Trim());
+        }
+
+        rowNodes = tableNode.SelectNodes(".//tr[position() > 1]");
+
+        foreach (var rowNode in rowNodes)
+        {
+            DataRow dataRow = dataTable.NewRow();
+            var cellNodes = rowNode.SelectNodes("./th|./td");
+
+            for (int i = 0; i < cellNodes.Count && i < dataTable.Columns.Count; i++)
+            {
+                dataRow[i] = cellNodes[i].InnerText.Trim().Replace('\u00A0', ' ').Trim();
+            }
+
+            dataTable.Rows.Add(dataRow);
+        }
+
+        // Rates table Flex
+        if (tableNode == null)
+            throw new Exception("Rates Flex table not found.");
+        var ratesFlex = new DataTable();
+
+        headerNodes = tableNode.SelectNodes(".//tr[1]/th");
+        foreach (var headerNode in headerNodes)
+        {
+            ratesFlex.Columns.Add(headerNode.InnerText.Trim());
+        }
+
+        rowNodes = tableNode.SelectNodes(".//tr[position() > 1]");
+
+        foreach (var rowNode in rowNodes)
+        {
+            DataRow dataRow = ratesFlex.NewRow();
+            var cellNodes = rowNode.SelectNodes("./th|./td");
+
+            for (int i = 0; i < cellNodes.Count && i < dataTable.Columns.Count; i++)
+            {
+                dataRow[i] = cellNodes[i].InnerText.Trim().Replace('\u00A0', ' ').Trim();
+            }
+
+            ratesFlex.Rows.Add(dataRow);
+        }
+
+        Console.WriteLine("");
     }
 }
